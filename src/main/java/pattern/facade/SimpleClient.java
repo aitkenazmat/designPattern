c70c2828448c648e1e49ff5809fdbb3fa9957437
a.aitken <azamat_93.93@mail.ru>
2018-12-17 14:45:19 +0600

    facade changed

--- src/main/java/pattern/facade/SimpleClient.java
+++ src/main/java/pattern/facade/SimpleClient.java
@@ -5,26 +5,20 @@ public class SimpleClient {
     public static void main(String[] args) {
 
         //1-й вариант без использование фасада
-//        CarBody carBody = new CarBody();
-//        //1
-//        carBody.setReadyCarBody(true);
-//        //2
-//        carBody.engineInstallation(new Engine());
-//        //3
-//        carBody.electroInstallation(new ElectricalAppliances());
-//        //4
-//        carBody.warmingCar();
-//        //5
-//        carBody.ustanovkaSeets(new Seats());
-//        //6
-//        carBody.mountingWheels(new Wheels());
-//
-//        Car car = new Car();
-//        car.setMarka("Kia");
-//        car.setCarBody(carBody);
-//        car.sendTestDrive();
+        CarBody carBody = new CarBody();
+        carBody.setReadyCarBody(true);
+        carBody.engineInstallation(new Engine());
+        carBody.electroInstallation(new ElectricalAppliances());
+        carBody.warmingCar();
+        carBody.ustanovkaSeets(new Seats());
+        carBody.mountingWheels(new Wheels());
 
-        //2- й вариант использование фасада
+        Car car = new Car();
+        car.setMarka("Kia");
+        car.setCarBody(carBody);
+        car.sendTestDrive();
+//-----------------------------------------------------------------------------------------------------------------
+        //2- й вариант с  использование фасада
         InstallationCarFacade installationCarFacade = new InstallationCarFacade();
         installationCarFacade.installationCar();
     }
