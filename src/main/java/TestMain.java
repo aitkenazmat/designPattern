8e67fcbd95b18f9c94940eb2d0dc85a82f8f0c3b
a.aitken <a.aitken@statdata.kz>
2019-03-16 10:33:38 +0600

    test

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -1,8 +1,15 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by WWW on 17.12.2018.
  */
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
     }
 }
